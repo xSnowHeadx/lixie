0d29b9b0167f167a508a7428063f07bb971dc944
xSnowHeadx <13079267+xSnowHeadx@users.noreply.github.com>
2019-04-01 19:37:11 +0200

    Sync improved no stuck at full minutes

--- Lixie.ino
+++ Lixie.ino
@@ -63,21 +63,14 @@ extern "C"
 #define NUM_DIGITS		   6
 #endif
 
-enum {
-	CMOD_NONE,
-	CMOD_STEADY,
-	CMOD_BLINK,
-	CMOD_ALTERNATE,
-	CMOD_AMPM,
-	CMOD_DATE,
-	CMOD_NUM_MODES,
+enum
+{
+	CMOD_NONE, CMOD_STEADY, CMOD_BLINK, CMOD_ALTERNATE, CMOD_AMPM, CMOD_DATE, CMOD_NUM_MODES,
 };
 
-enum {
-	DMOD_NONE,
-	DMOD_MINUTE,
-	DMOD_HOUR,
-	DMOD_NOW
+enum
+{
+	DMOD_NONE, DMOD_MINUTE, DMOD_HOUR, DMOD_NOW
 };
 
 #define DATA_PIN 	 14			  // output-pin for LED-data (current D5)
@@ -107,7 +100,8 @@ Timezone myTZ(myDST, mySTD);
 #endif
 #define CONSTRAIN(a, l, r)    (MIN(MAX((l), (a)), (r)))
 
-typedef struct {
+typedef struct
+{
 	float r;
 	float g;
 	float b;
@@ -128,89 +122,86 @@ unsigned int datemode = 0;
 // number order from front to back: 1 0 9 2 7 8 3 6 5 4
 /*
 
-Number			LED position
-    4       4    	   009---->|     019
-5 	 	5 	 	000     |     010     |
- 	6	 	6	 |	   008	   |	 018
-3	 	3	 	001	    |	  011	  |
- 	8	 	8	 |	   007	   |	 017
-7	 	7	 	002     |	  012	  |
- 	2	 	2	 |	   006	   |	 016
-9	 	9	 	003     |	  013	  |
- 	0	 	0	 |	   005	   |	 015
-1	 	1	 	004---->|     014---->|
+ Number			LED position
+ 4       4    	   009---->|     019
+ 5 	 	5 	 	000     |     010     |
+ 6	 	6	 |	   008	   |	 018
+ 3	 	3	 	001	    |	  011	  |
+ 8	 	8	 |	   007	   |	 017
+ 7	 	7	 	002     |	  012	  |
+ 2	 	2	 |	   006	   |	 016
+ 9	 	9	 	003     |	  013	  |
+ 0	 	0	 |	   005	   |	 015
+ 1	 	1	 	004---->|     014---->|
  */
 
-static const unsigned long ledmap[10] = {
-		0b00001000000000100000,
-		0b00000100000000010000,
-		0b00010000000001000000,
-		0b00000000100000000010,
-		0b10000000001000000000,
-		0b00000000010000000001,
-		0b01000000000100000000,
-		0b00000001000000000100,
-		0b00100000000010000000,
-		0b00000010000000001000,
+static const unsigned long ledmap[10] =
+{
+	0b00001000000000100000,
+	0b00000100000000010000,
+	0b00010000000001000000,
+	0b00000000100000000010,
+	0b10000000001000000000,
+	0b00000000010000000001,
+	0b01000000000100000000,
+	0b00000001000000000100,
+	0b00100000000010000000,
+	0b00000010000000001000,
 };
 
 #else
 
 // number order from front to back: 1 2 3 4 5 6 7 8 9 0
 /*
-Number			LED position
-    0       0    	   009---->|     019
-9 	 	9 	 	000     |     010     |
- 	8	 	8	 |	   008	   |	 018
-7	 	7	 	001	    |	  011	  |
- 	6	 	6	 |	   007	   |	 017
-5	 	5	 	002     |	  012	  |
- 	4	 	4	 |	   006	   |	 016
-3	 	3	 	003     |	  013	  |
- 	2	 	2	 |	   005	   |	 015
-1	 	1	 	004---->|     014---->|
+ Number			LED position
+ 0       0    	   009---->|     019
+ 9 	 	9 	 	000     |     010     |
+ 8	 	8	 |	   008	   |	 018
+ 7	 	7	 	001	    |	  011	  |
+ 6	 	6	 |	   007	   |	 017
+ 5	 	5	 	002     |	  012	  |
+ 4	 	4	 |	   006	   |	 016
+ 3	 	3	 	003     |	  013	  |
+ 2	 	2	 |	   005	   |	 015
+ 1	 	1	 	004---->|     014---->|
  */
-static const unsigned long ledmap[10] = {
+static const unsigned long ledmap[10] =
+{
 
-		0b10000000001000000000,
-		0b00000100000000010000,
-		0b00001000000000100000,
-		0b00000010000000001000,
-		0b00010000000001000000,
-		0b00000001000000000100,
-		0b00100000000010000000,
-		0b00000000100000000010,
-		0b01000000000100000000,
-		0b00000000010000000001,
-};
+0b10000000001000000000, 0b00000100000000010000, 0b00001000000000100000, 0b00000010000000001000, 0b00010000000001000000,
+		0b00000001000000000100, 0b00100000000010000000, 0b00000000100000000010, 0b01000000000100000000,
+		0b00000000010000000001, };
 #endif
 
-static const float corr_digit[NUM_DIGIT_LEDS / 2] = {
+static const float corr_digit[NUM_DIGIT_LEDS / 2] =
+{
 //      9	  7	    5	  3     1
 		1.00, 1.0, 0.90, 0.80, 0.85,
 //      2	  4	    6	  8     0
-		0.75, 0.75, 0.94, 0.96, 1.00
-};
+		0.75, 0.75, 0.94, 0.96, 1.00 };
 
 #if HAS_COLONS
-static const unsigned char colonmap[CMOD_NUM_MODES] = {
-		0b00000000,	// CMOD_NOTHING
+static const unsigned char colonmap[CMOD_NUM_MODES] =
+{ 0b00000000,	// CMOD_NOTHING
 		0b00110011, // CMOD_STEADY
 		0b00000011, // CMOD_BLINK
 		0b00100001, // CMOD_ALTERNATE
 		0b00001001, // CMOD_AMPM
 		0b00010001, // CMOD_DATE
-};
+		};
 
-static const float corr_colon[NUM_COLON_LEDS] = {
+static const float corr_colon[NUM_COLON_LEDS] =
+{
 //      COL-B COL-T
-		0.70, 0.70
-};
+		0.70, 0.70 };
 #endif
 
 void update_timeleds(void);
 
-enum {COLORMODE_FIX, COLORMODE_RBFULL, COLORMODE_RBFIX, COLORMODE_RBSHIFT};
+enum
+{
+	COLORMODE_FIX, COLORMODE_RBFULL, COLORMODE_RBFIX, COLORMODE_RBSHIFT
+};
 
 // structure holds the parameters to save nonvolatile
 typedef struct
@@ -228,10 +219,7 @@ lixie_processor_struct lixie_processor;
 lixie_processor_struct *lixiep = &lixie_processor;
 
 // float parameters for rainbow-mode
-float rainbow_offset = 0.0, rainbow_step = 0.000006, rainbow_posstep = 1.0 / (float)NUM_DIGITS;
-
-// counter for time-measurement
-static unsigned long amicros, umicros = 0;
+float rainbow_offset = 0.0, rainbow_step = 0.000006, rainbow_posstep = 1.0 / (float) NUM_DIGITS;
 
 // LED-array for transfer to FastLED
 CRGB leds[NUM_LEDS];
@@ -276,25 +264,25 @@ int get_digit_pos(int dledpos)
 #if ! HAS_COLONS
 	digitpos = i / NUM_DIGIT_LEDS;
 #else
-	if(dledpos >= NUM_DIGIT_LEDS)
+	if (dledpos >= NUM_DIGIT_LEDS)
 	{
 		rval = 1;
-		if(dledpos >= 2 * NUM_DIGIT_LEDS)
+		if (dledpos >= 2 * NUM_DIGIT_LEDS)
 		{
 			rval = 2;
-			if(dledpos >= 2 * NUM_DIGIT_LEDS + NUM_COLON_LEDS)
+			if (dledpos >= 2 * NUM_DIGIT_LEDS + NUM_COLON_LEDS)
 			{
 				rval = 3;
-				if(dledpos >= 3 * NUM_DIGIT_LEDS + NUM_COLON_LEDS)
+				if (dledpos >= 3 * NUM_DIGIT_LEDS + NUM_COLON_LEDS)
 				{
 					rval = 4;
-					if(dledpos >= 4 * NUM_DIGIT_LEDS + NUM_COLON_LEDS)
+					if (dledpos >= 4 * NUM_DIGIT_LEDS + NUM_COLON_LEDS)
 					{
 						rval = 5;
-						if(dledpos >= 4 * NUM_DIGIT_LEDS + 2 * NUM_COLON_LEDS)
+						if (dledpos >= 4 * NUM_DIGIT_LEDS + 2 * NUM_COLON_LEDS)
 						{
 							rval = 6;
-							if(dledpos >= 5 * NUM_DIGIT_LEDS + 2 * NUM_COLON_LEDS)
+							if (dledpos >= 5 * NUM_DIGIT_LEDS + 2 * NUM_COLON_LEDS)
 							{
 								rval = 7;
 							}
@@ -372,52 +360,51 @@ void update_rainbow(unsigned char dpos)
 {
 	switch (lixiep->mode)
 	{
-		case COLORMODE_FIX:
-		{
-			// generate color-structure for wished display-color
-			actcolor = CRGB(((int) lixiep->r * (int) lixiep->brightness) / 40,
-					((int) lixiep->g * (int) lixiep->brightness) / 40,
-					((int) lixiep->b * (int) lixiep->brightness) / 40);
-		}
+	case COLORMODE_FIX:
+	{
+		// generate color-structure for wished display-color
+		actcolor = CRGB(((int) lixiep->r * (int) lixiep->brightness) / 40,
+				((int) lixiep->g * (int) lixiep->brightness) / 40, ((int) lixiep->b * (int) lixiep->brightness) / 40);
+	}
 		break;
 
-		case COLORMODE_RBFULL:
-		{
-			int r, g, b;
+	case COLORMODE_RBFULL:
+	{
+		int r, g, b;
 
-			// calulate color for this loop
-			hsl_to_rgb((int)(255.0 * rainbow_offset), 255, 128, &r, &g, &b);
+		// calulate color for this loop
+		hsl_to_rgb((int) (255.0 * rainbow_offset), 255, 128, &r, &g, &b);
 
-			// generate color-structure for wished display-color
-			actcolor = CRGB((r * (int) lixiep->brightness) / 100,
-					(g * (int) lixiep->brightness) / 100, (b * (int) lixiep->brightness) / 100);
-		}
+		// generate color-structure for wished display-color
+		actcolor = CRGB((r * (int) lixiep->brightness) / 100, (g * (int) lixiep->brightness) / 100,
+				(b * (int) lixiep->brightness) / 100);
+	}
 		break;
 
-		case COLORMODE_RBFIX:
-		{
-			int r, g, b;
+	case COLORMODE_RBFIX:
+	{
+		int r, g, b;
 
-			// calulate color for this loop
-			hsl_to_rgb((int)(255.0 * xfmod((dpos * rainbow_posstep), 1.0)), 255, 128, &r, &g, &b);
+		// calulate color for this loop
+		hsl_to_rgb((int) (255.0 * xfmod((dpos * rainbow_posstep), 1.0)), 255, 128, &r, &g, &b);
 
-			// generate color-structure for wished display-color
-			actcolor = CRGB((r * (int) lixiep->brightness) / 100,
-					(g * (int) lixiep->brightness) / 100, (b * (int) lixiep->brightness) / 100);
-		}
+		// generate color-structure for wished display-color
+		actcolor = CRGB((r * (int) lixiep->brightness) / 100, (g * (int) lixiep->brightness) / 100,
+				(b * (int) lixiep->brightness) / 100);
+	}
 		break;
 
-		case COLORMODE_RBSHIFT:
-		{
-			int r, g, b;
+	case COLORMODE_RBSHIFT:
+	{
+		int r, g, b;
 
-			// calulate color for this loop
-			hsl_to_rgb((int)(255.0 * xfmod(dpos * rainbow_posstep + rainbow_offset, 1.0)), 255, 128, &r, &g, &b);
+		// calulate color for this loop
+		hsl_to_rgb((int) (255.0 * xfmod(dpos * rainbow_posstep + rainbow_offset, 1.0)), 255, 128, &r, &g, &b);
 
-			// generate color-structure for wished display-color
-			actcolor = CRGB((r * (int) lixiep->brightness) / 100,
-					(g * (int) lixiep->brightness) / 100, (b * (int) lixiep->brightness) / 100);
-		}
+		// generate color-structure for wished display-color
+		actcolor = CRGB((r * (int) lixiep->brightness) / 100, (g * (int) lixiep->brightness) / 100,
+				(b * (int) lixiep->brightness) / 100);
+	}
 		break;
 	}
 }
@@ -427,9 +414,9 @@ void num2led(int *ledpos, int number)
 	int i;
 	unsigned long mask = 0b00000000000000000001;
 
-	for(i = 0; i < NUM_DIGIT_LEDS; i++)
+	for (i = 0; i < NUM_DIGIT_LEDS; i++)
 	{
-		if(mask & ledmap[number])
+		if (mask & ledmap[number])
 		{
 			leds_curr[*ledpos + i] = 1.0;
 		}
@@ -447,20 +434,20 @@ void colon2led(int *ledpos, unsigned char even, unsigned char ampm)
 	int i, cmode = lixiep->cmode;
 	unsigned char mask = 0b00000001;
 
-	if(!even)
+	if (!even)
 		mask <<= 4;
-	if(datemode)
+	if (datemode)
 	{
 		cmode = CMOD_DATE;
 	}
 	else
 	{
-		if((lixiep->cmode == CMOD_AMPM) && ampm)
+		if ((lixiep->cmode == CMOD_AMPM) && ampm)
 			mask <<= 2;
 	}
-	for(i = 0; i < NUM_COLON_LEDS; i++)
+	for (i = 0; i < NUM_COLON_LEDS; i++)
 	{
-		if(mask & colonmap[cmode])
+		if (mask & colonmap[cmode])
 		{
 			leds_curr[*ledpos + i] = 1.0;
 		}
@@ -473,39 +460,40 @@ void colon2led(int *ledpos, unsigned char even, unsigned char ampm)
 	*ledpos += NUM_COLON_LEDS;
 }
 
-void sweep(void) {
+void sweep(void)
+{
 	int i, digitpos = 0, corrpos;
 	float corrfact;
 
-	if(datemode)
+	if (datemode)
 		--datemode;
-	for(i = 0; i < NUM_LEDS; i++)
+	for (i = 0; i < NUM_LEDS; i++)
 	{
-		if(leds_last[i] < leds_curr[i])
+		if (leds_last[i] < leds_curr[i])
 		{
 			leds_last[i] += leds_step;
 		}
-		else if(leds_last[i] > leds_curr[i])
+		else if (leds_last[i] > leds_curr[i])
 		{
 			leds_last[i] -= leds_step;
 		}
-		if((leds_last[i] < 0.0) || (leds_last[i] < leds_step))
+		if ((leds_last[i] < 0.0) || (leds_last[i] < leds_step))
 			leds_last[i] = 0.0;
 		digitpos = get_digit_pos(i);
 #if ! HAS_COLONS
 		corrpos = i % NUM_DIGIT_LEDS;
 		corrfact = corr_digit[corrpos];
 #else
-		if((digitpos == 2) || (digitpos == 5)) // colons
+		if ((digitpos == 2) || (digitpos == 5)) // colons
 		{
 			corrfact = corr_colon[i & 1];
 		}
 		else
 		{
 			corrpos = i;
-			if(digitpos >= 2) // colon 1
+			if (digitpos >= 2) // colon 1
 				corrpos -= 2;
-			if(digitpos >= 5) // colon 2
+			if (digitpos >= 5) // colon 2
 				corrpos -= 2;
 			corrpos = corrpos % (NUM_DIGIT_LEDS / 2);
 			corrfact = corr_digit[corrpos];
@@ -513,9 +501,9 @@ void sweep(void) {
 #endif
 		rainbow_offset = xfmod(rainbow_offset + rainbow_step, 1.0);
 		update_rainbow(digitpos);
-		leds[i].r = (int)((float)actcolor.r * leds_last[i] * corrfact);
-		leds[i].g = (int)((float)actcolor.g * leds_last[i] * corrfact);
-		leds[i].b = (int)((float)actcolor.b * leds_last[i] * corrfact);
+		leds[i].r = (int) ((float) actcolor.r * leds_last[i] * corrfact);
+		leds[i].g = (int) ((float) actcolor.g * leds_last[i] * corrfact);
+		leds[i].b = (int) ((float) actcolor.b * leds_last[i] * corrfact);
 #if ! _FASTLED_
 		strip.setPixelColor(i, leds[i].r, leds[i].g, leds[i].b);
 #endif
@@ -530,7 +518,7 @@ void sweep(void) {
 // dynamic generation of the web-servers response
 void page_out(void)
 {
-	if (strlen (outstr))
+	if (strlen(outstr))
 	{
 		server.sendContent(outstr);
 		*outstr = 0;
@@ -560,7 +548,7 @@ void page_out(void)
 		server.sendContent(">Rainbow sweeping</option></select></td></tr>");
 #if HAS_COLONS
 		server.sendContent(
-			"<tr><td width=\"120\"><b><big>Colons mode</big></b></td><td width=\"50\"></td><td><select name=\"CMODE\"><option value=\"0\"");
+				"<tr><td width=\"120\"><b><big>Colons mode</big></b></td><td width=\"50\"></td><td><select name=\"CMODE\"><option value=\"0\"");
 		if (lixiep->cmode == CMOD_NONE)
 			server.sendContent(" selected");
 		server.sendContent(">no colons</option><option value=\"1\"");
@@ -578,7 +566,7 @@ void page_out(void)
 		server.sendContent(">AM/PM</option></select></td></tr>");
 #endif
 		server.sendContent(
-			"<tr><td width=\"120\"><b><big>Date mode</big></b></td><td width=\"50\"></td><td><select name=\"DMODE\"><option value=\"0\"");
+				"<tr><td width=\"120\"><b><big>Date mode</big></b></td><td width=\"50\"></td><td><select name=\"DMODE\"><option value=\"0\"");
 		if (lixiep->dmode == DMOD_NONE)
 			server.sendContent(" selected");
 		server.sendContent(">inactive</option><option value=\"1\"");
@@ -622,7 +610,7 @@ void setup()
 	wifiManager.setTimeout(30);
 
 	// initialize FastLED, all LEDs off
-	memset(leds,      0, sizeof(CRGB) * NUM_LEDS);
+	memset(leds, 0, sizeof(CRGB) * NUM_LEDS);
 	memset(leds_last, 0, sizeof(float) * NUM_LEDS);
 	memset(leds_curr, 0, sizeof(float) * NUM_LEDS);
 
@@ -630,11 +618,11 @@ void setup()
 	FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
 	FastLED.show();
 #else
-	  strip.begin();
-	  strip.show(); // Initialize all pixels to 'off'
-	  strip.setBrightness(255);
+	strip.begin();
+	strip.show(); // Initialize all pixels to 'off'
+	strip.setBrightness(255);
 #endif
-	 // read stored parameters
+	// read stored parameters
 	EEPROM.begin(EEPROM_SIZE);
 	eptr = (unsigned char*) lixiep;
 	for (i = 0; i < sizeof(lixie_processor_struct); i++)
@@ -715,11 +703,11 @@ void setup()
 				else if(server.argName(i) == "HMOD24")
 				{
 					if(server.arg(i) == "on")
-						lixiep->h24 = 1;
+					lixiep->h24 = 1;
 					else if(server.arg(i) == "off")
-						lixiep->h24 = 0;
+					lixiep->h24 = 0;
 					else
-						sprintf(outstr + strlen(outstr), "HMOD24=%s\r\n", (lixiep->h24)?"on":"off");
+					sprintf(outstr + strlen(outstr), "HMOD24=%s\r\n", (lixiep->h24)?"on":"off");
 				}
 				else if(server.argName(i) == "MODE")
 				{
@@ -728,10 +716,10 @@ void setup()
 					{
 						lixiep->mode = pval;
 						if(lixiep->mode > 3)
-							lixiep->mode = 3;
+						lixiep->mode = 3;
 					}
 					else
-						sprintf(outstr + strlen(outstr), "MODE=%d\r\n", lixiep->mode);
+					sprintf(outstr + strlen(outstr), "MODE=%d\r\n", lixiep->mode);
 				}
 				else if(server.argName(i) == "CMODE")
 				{
@@ -740,10 +728,10 @@ void setup()
 					{
 						lixiep->cmode = pval;
 						if(lixiep->cmode > 4)
-							lixiep->cmode = 4;
+						lixiep->cmode = 4;
 					}
 					else
-						sprintf(outstr + strlen(outstr), "CMODE=%d\r\n", lixiep->mode);
+					sprintf(outstr + strlen(outstr), "CMODE=%d\r\n", lixiep->mode);
 				}
 				else if(server.argName(i) == "DMODE")
 				{
@@ -751,12 +739,12 @@ void setup()
 					if(server.arg(i).length() && !errno)
 					{
 						if(pval >= DMOD_NOW)
-							datemode = DATE_TIME * 100;
+						datemode = DATE_TIME * 100;
 						else
-							lixiep->dmode = pval;
+						lixiep->dmode = pval;
 					}
 					else
-						sprintf(outstr + strlen(outstr), "DMODE=%d\r\n", lixiep->dmode);
+					sprintf(outstr + strlen(outstr), "DMODE=%d\r\n", lixiep->dmode);
 				}
 				else if(server.argName(i) == "BRIGHT")
 				{
@@ -777,9 +765,9 @@ void setup()
 					{
 						lixiep->fade = pval;
 						if(lixiep->fade > 80)
-							lixiep->fade = 80;
+						lixiep->fade = 80;
 						if(lixiep->fade < 1)
-							lixiep->fade = 1;
+						lixiep->fade = 1;
 						leds_step = 1.0 / lixiep->fade;
 					}
 					else
@@ -835,7 +823,7 @@ void setup()
 
 void update_timeleds(void)
 {
-	static int lsec = 61, min, lmin = -1;
+	static int lsec = 61, min;
 	int sec, sec1, sec10, min1, min10, hr1, hr10, hr24, ledpos;
 	time_t rawtime, loctime;
 
@@ -845,81 +833,77 @@ void update_timeleds(void)
 	min = minute(loctime);
 	hr24 = hour(loctime);
 
-	if ((min != lmin) && timeClient.update())						// NTP-update
+	if (!synced || (!hr24 && !min && !sec))		// sync if midnight or not synced
+		synced = timeClient.update();			// NTP-update
+	if (!lixiep->h24 && (hr24 >= 12))
+		hr24 -= 12;
+	if (sec != lsec)
 	{
-		umicros = amicros;++
-		lmin = min;
-	}
-	if(synced)
-	{
-		if (!lixiep->h24 && (hr24 >= 12))
-			hr24 -= 12;
-		if (sec != lsec)
+		if (lixiep->dmode)
 		{
-			if(lixiep->dmode)
+			if (!sec)
 			{
-				if(!sec)
+				if ((lixiep->dmode == DMOD_MINUTE) || !min)
 				{
-					if((lixiep->dmode == DMOD_MINUTE) || !min)
-					{
-						datemode = DATE_TIME * 100;
-					}
+					datemode = DATE_TIME * 100;
 				}
 			}
+		}
 
-			lsec = sec;
+		lsec = sec;
 
-			if(datemode)
-			{
-				sec = year(loctime) - 2000;
-				min = month(loctime);
-				hr24 = day(loctime);
-			}
+		if (datemode)
+		{
+			sec = year(loctime) - 2000;
+			min = month(loctime);
+			hr24 = day(loctime);
+		}
 
-			sec1 = sec % 10;
+		sec1 = sec % 10;
+
+		if (synced)
+		{
 			sec10 = sec / 10;
 			min1 = min % 10;
 			min10 = min / 10;
 			hr1 = hr24 % 10;
 			hr10 = hr24 / 10;
-
-			ledpos = 0;
-
-			num2led(&ledpos,  hr10);
-			num2led(&ledpos,  hr1);
-#if HAS_COLONS
-			colon2led(&ledpos, sec1 & 1, hour(loctime) >= 12);
-#endif
-			num2led(&ledpos,  min10);
-			num2led(&ledpos,  min1);
-#if HAS_COLONS
-			colon2led(&ledpos, sec1 & 1,  hour(loctime) >= 12);
-#endif
-			num2led(&ledpos,  sec10);
-			num2led(&ledpos,  sec1);
-
 			digitalWrite(BUILTIN_LED, HIGH);		// blink for sync
 		}
-		delay(10);
-		digitalWrite(BUILTIN_LED, LOW);
+		else
+		{
+			sec10 = sec1;
+			min1 = sec1;
+			min10 = sec1;
+			hr1 = sec1;
+			hr10 = sec1;
+		}
+
+		ledpos = 0;
+
+		num2led(&ledpos, hr10);
+		num2led(&ledpos, hr1);
+#if HAS_COLONS
+		colon2led(&ledpos, sec1 & 1, hour(loctime) >= 12);
+#endif
+		num2led(&ledpos, min10);
+		num2led(&ledpos, min1);
+#if HAS_COLONS
+		colon2led(&ledpos, sec1 & 1, hour(loctime) >= 12);
+#endif
+		num2led(&ledpos, sec10);
+		num2led(&ledpos, sec1);
+
 	}
-	else
-		delay(10);
+	delay(10);
+	digitalWrite(BUILTIN_LED, LOW);
 }
 
 void loop()
 {
 	// do the button functions
 	d_mode.tick();
-	amicros = micros();
 	update_timeleds();
 	server.handleClient();						// handle HTTP-requests
-	if (((amicros - umicros) / 1000000L) > 36000)							// if no sync for more than ten hours
-	{
-		digitalWrite(BUILTIN_LED, HIGH);		// switch off BUILTIN_LED
-		synced = 0;
-	}
-	else
-		synced = 1;
 }
 
