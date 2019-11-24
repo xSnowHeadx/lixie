3a26880f3332f01876289874985d53e219d86b9e
xSnowHeadx <13079267+xSnowHeadx@users.noreply.github.com>
2019-11-24 12:34:11 +0100

    numeric output of nightmode times in webinterface simplified

--- Lixie.ino
+++ Lixie.ino
@@ -43,7 +43,7 @@ extern "C"
 
 // begin of individual settings
 
-#define HAS_COLONS		   1 	// set to 0 if no colon digits used
+#define HAS_COLONS		   0 	// set to 0 if no colon digits used
 
 #define DATE_TIME		   4    // show date for 4 seconds
 
@@ -490,7 +490,7 @@ void sweep(void)
 			leds_last[i] -= leds_step;
 		}
     if(leds_last[i] > 1.0)
-      leds_last[i] =1.0;
+      leds_last[i] = 1.0;
    
 		if ((leds_last[i] < 0.0) || (leds_last[i] < leds_step))
 			leds_last[i] = 0.0;
@@ -600,10 +600,10 @@ void page_out(void)
 		server.sendContent(String(lixiep->nm_brightness));
 		server.sendContent(
 				"\"> %</td><td>Start<br><input maxlength=\"3\" size=\"3\" name=\"NMSTART\" value=\"");
-    server.sendContent((lixiep->nm_start > 9)? String(lixiep->nm_start) :"0" + String(lixiep->nm_start));
+    server.sendContent(((lixiep->nm_start > 9)? "" : "0") + String(lixiep->nm_start));
     server.sendContent(
         "\"> h</td><td>End<br><input maxlength=\"3\" size=\"3\" name=\"NMEND\" value=\"");
-    server.sendContent((lixiep->nm_end > 9)? String(lixiep->nm_end) :"0" + String(lixiep->nm_end));
+    server.sendContent(((lixiep->nm_end > 9)? "" : "0") + String(lixiep->nm_end));
 		server.sendContent(
 				"\"> h</td></tr><tr><td> <br></td></tr><tr><td><b><big>Fading</font></big></b></td><td></td><td><input maxlength=\"3\" size=\"3\" name=\"FADE\" value=\"");
 		server.sendContent(String(lixiep->fade));
